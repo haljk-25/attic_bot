a396ba3303253b01f1f1589250c59a212fc609be
jules.kai <jules.kai.hall@gmail.com>
2022-10-02 21:28:22 -0500

    attempted to add commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -12,6 +12,7 @@ for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
 	const command = require(filePath);
 	commands.push(command.data.toJSON());
+}
 
 const rest = new REST({ version: '10' }).setToken(token);
 
@@ -19,4 +20,3 @@ rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
 	.then((data) => console.log(`Successfully registered ${data.length} application commands.`))
 	.catch(console.error);
 
-}
