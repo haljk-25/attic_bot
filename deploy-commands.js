6f492b3e7da76b405eaefa8cc98e6308619613f1
jules.kai <jules.kai.hall@gmail.com>
2022-10-02 19:37:05 -0500

    bug squashing a misplaced curlybrace ToT

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,12 +1,17 @@
-const { REST, SlashCommandBuilder, Routes } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { REST, Routes } = require('discord.js');
 const { clientId, guildId, token } = require('./config.json');
 
-const commands = [
-	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
-]
-	.map(command => command.toJSON());
+const commands = []
+const commandsPath = path.join(__dirname, 'commands'); // commands dir
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js')); // filter non .js files
+
+// add all command files to array of commands
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	commands.push(command.data.toJSON());
 
 const rest = new REST({ version: '10' }).setToken(token);
 
@@ -14,4 +19,4 @@ rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
 	.then((data) => console.log(`Successfully registered ${data.length} application commands.`))
 	.catch(console.error);
 
- 
+}
--- discordbot.js
+++ discordbot.js
@@ -1,29 +1,46 @@
 // Require the necessary discord.js classes
-const { Client, GatewayIntentBits } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+
+// access commands:
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands'); // path to commands dir
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js')); // filter non .js files
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
+
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isChatInputCommand()) return;
-
-	const { commandName } = interaction;
-
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
-	} else if (commandName === 'server') {
-		await interaction.reply(`Server info:\nServer name: ${interaction.guild.name}\nMembers: ${interaction.guild.memberCount}`);
-	} else if (commandName === 'user') {
-		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
-	} 
+	
+	const command = interaction.client.commands.get(interaction.commandName);
+	
+	if (!command) return;
+	
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
 });
 
 // Login to Discord with your client's token
 client.login(token);
-
